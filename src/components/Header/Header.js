a4b5d55365cca6a7d0ed35663fce73bee1a75064
Nikolai <niklonberg@gmail.com>
2024-04-11 12:13:57 +0200

    add starting header component logic

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React from "react";
 
-function Header() {
-  return <div></div>;
+function Header({ title, headingLevel = 1 }) {
+  const HeadingLvlTag = `h${headingLevel}`;
+  if (typeof headingLevel !== "number" || headingLevel < 1 || headingLevel > 6)
+    throw Error(
+      `Unrecognized value for '${headingLevel}'. Expected number between 1 - 6`
+    );
+  return (
+    <header>
+      <HeadingLvlTag>{title}</HeadingLvlTag>
+    </header>
+  );
 }
 
 export default Header;
--- src/components/ToastingGround/ToastingGround.js
+++ src/components/ToastingGround/ToastingGround.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React from "react";
+import Header from "../Header";
 
 function ToastingGround() {
-  return <div></div>;
+  return (
+    <div>
+      <Header title="Try-My-Toaster!" />
+    </div>
+  );
 }
 
 export default ToastingGround;
