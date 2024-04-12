265bc1777ae60e918a0b5f72988be7699db89e14
Nikolai <niklonberg@gmail.com>
2024-04-12 13:00:47 +0200

    edit Input component to be able to create textarea instead of input if needed

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -4,6 +4,14 @@ function Input({ id, label, inputType, ...delegated }) {
   const generatedId = React.useId();
   const appliedId = id | generatedId;
 
+  if (inputType === "textarea")
+    return (
+      <>
+        <label htmlFor={appliedId}>{label}</label>
+        <textarea id={appliedId} {...delegated}></textarea>
+      </>
+    );
+
   return (
     <>
       <label htmlFor={appliedId}>{label}</label>
