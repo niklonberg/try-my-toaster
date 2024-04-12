614be3f7c3e604870ad7ed2253017fad51c012f8
Nikolai <niklonberg@gmail.com>
2024-04-12 15:08:29 +0200

    add support for callback function prop to run on Form submit

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,8 +1,21 @@
 import React from "react";
 import styles from "./Form.module.scss";
 
-function Form({ children }) {
-  return <form className={styles.wrapper}>{children}</form>;
+function Form({ children, callback }) {
+  function defaultSubmit() {
+    console.log("form submitted");
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    (callback || defaultSubmit)();
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.wrapper}>
+      {children}
+    </form>
+  );
 }
 
 export default Form;
