66dd9751c2b174d7e55304bad2a8489c3603a38f
Nikolai <niklonberg@gmail.com>
2024-04-12 12:51:49 +0200

    add Input component

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React from "react";
 
-function Input() {
-  return <div></div>;
+function Input({ id, label, inputType, ...delegated }) {
+  const generatedId = React.useId();
+  const appliedId = id | generatedId;
+
+  return (
+    <>
+      <label htmlFor={appliedId}>{label}</label>
+      <input id={appliedId} type={inputType} {...delegated}></input>
+    </>
+  );
 }
 
 export default Input;
