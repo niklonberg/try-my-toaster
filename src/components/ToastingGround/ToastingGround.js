58d517967f167fa774ab79f93369b405b04f6640
Nikolai <niklonberg@gmail.com>
2024-04-14 11:37:57 +0200

    add toasts state, handleAddToast function

--- src/components/ToastingGround/ToastingGround.js
+++ src/components/ToastingGround/ToastingGround.js
@@ -4,10 +4,19 @@ import FormToastingGround from "../Form/FormToastMaker";
 import styles from "./ToastingGround.module.scss";
 
 function ToastingGround() {
+  const [toasts, setToasts] = React.useState([]);
+
+  function handleAddToast(variant, message) {
+    const nextToasts = [...toasts, { variant, message }];
+    setToasts(nextToasts);
+  }
+
+  console.log({ toasts });
   return (
     <div className={styles.wrapper}>
       <HeaderToastingGround />
-      <FormToastingGround />
+
+      <FormToastingGround handleAddToast={handleAddToast} />
     </div>
   );
 }
