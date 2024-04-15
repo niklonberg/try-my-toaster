6e894005183ccc70fe589c99a92fe1d87c3772f2
Nikolai <niklonberg@gmail.com>
2024-04-15 14:13:25 +0200

    fix import

--- src/components/VisuallyHidden/VisuallyHidden.js
+++ src/components/VisuallyHidden/VisuallyHidden.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import styles from "./VisuallyHidden.module.css";
+import styles from "./VisuallyHidden.module.scss";
 
 const VisuallyHidden = ({ children, className = "", ...delegated }) => {
   const [forceShow, setForceShow] = React.useState(false);
