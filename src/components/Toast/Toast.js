e3274f6d7759916ff57e59aab3c36ed4d7cab2ca
Nikolai <niklonberg@gmail.com>
2024-04-15 09:01:30 +0200

    add toast icon imports

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { X } from "react-feather";
+import {
+  X,
+  Info,
+  CheckCircle,
+  AlertOctagon,
+  AlertTriangle,
+} from "react-feather";
 
 import styles from "./Toast.module.scss";
 
@@ -7,11 +13,13 @@ function Toast({ toast }) {
   const { variant, message } = toast;
   return (
     <div className={styles.wrapper}>
-      {/* <svg></svg> */}
-      <span>IconVariantHere</span>
+      <svg></svg>
       <span>{message}</span>
       <button>
         <X />
+      </button>
+      <button>
+        <AlertTriangle />
       </button>
     </div>
   );
